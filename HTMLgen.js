3ce06754b3fa653f3abbc9502c87e09c5da7724e
the-electro-punk <bwr2555@hotmail.com>
2021-10-22 17:08:44 -0500

    downloaded node html and updated htmlgen

--- HTMLgen.js
+++ HTMLgen.js
@@ -2,26 +2,11 @@ const fs = require('fs')
 const inquirer = require('inquirer');
 inquirer.registerPrompt('loop', require('inquirer-loop')(inquirer));
 
-var http = require('http')
+var fileName = 'C:/Users/Ben/Desktop/_Coding BootCamp/_weeks lessons/week 10/EmployeeData/HTMLgen.html';
+var stream = fs.createWriteStream(fileName);
 
-http.createServer(function (req, res) {
-    var html = buildHtml(req);
-  
-    res.writeHead(200, {
-      'Content-Type': 'text/html',
-      'Content-Length': html.length,
-      'Expires': new Date().toUTCString()
-    });
-    res.end(html);
-  }).listen(8080);
-  
-  function buildHtml(req) {
-    var header = '';
-    var body = '';
-  
-    // concatenate header string
-    // concatenate body string
-  
-    return '<!DOCTYPE html>'
-         + '<html><head>' + header + '</head><body>' + body + '</body></html>';
-  };
+stream.once('open', function(fd) {
+  var html = buildHtml();
+
+  stream.end(html);
+});
