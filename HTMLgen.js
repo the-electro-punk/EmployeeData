3af74077328b5fcc519331fa3323f4f2e6e83abc
the-electro-punk <bwr2555@hotmail.com>
2021-10-22 13:02:22 -0500

    made minor tweaks

--- HTMLgen.js
+++ HTMLgen.js
@@ -1,3 +1,36 @@
+const fs = require('fs')
+const inquirer = require('inquirer');
+inquirer.registerPrompt('loop', require('inquirer-loop')(inquirer));
 
 
-const empHTML = document.implementation.createHTMLDocument()
+inquirer.prompt(
+    [
+        {
+        type: 'input', 
+        name:"input_name", 
+        message:'what is filename.md?',
+        validate: (answer) => {
+            if(answer === '') {
+                return 'please enter a valid name'
+            }
+                return true
+            }
+        },
+        {
+        type:'input',
+        name:'content_question',
+        message: 'what will the ReadMe file contain?',
+        validate: (answer) => {
+            if(answer === '') {
+                    return 'please enter a valid name'
+            }
+                return true
+            }
+        },
+    ]
+).then(answers => {
+    const fileName = answers.input_name;
+    const content = answers.content_question;
+
+    const empHTML = document.implementation.createHTMLDocument(fileName)
+})
