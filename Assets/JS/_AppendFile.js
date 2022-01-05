324a0ae7f4dc747f4643c0ed9540ad276384e6f2
the-electro-punk <88555154+the-electro-punk@users.noreply.github.com>
2022-01-05 16:20:07 -0600

    created better inquirer code and practiced it

--- Assets/JS/_AppendFile.js
+++ Assets/JS/_AppendFile.js
@@ -99,26 +99,12 @@ inquirer.prompt({
         const htEmp = answers.items[i].height_question;
         const jobEmp = answers.items[i].role_question;
 
-        // this creates the basic html setup
-        const htmlBase = '<!DOCTYPE html>' + '\n' + '<html lang="en">' + '\n' + '<head>' + '\n' + '<meta charset="UTF-8">' + '\n' + '<meta http-equiv="X-UA-Compatible" content="IE=edge">' + '\n' + '<meta name="viewport" content="width=device-width, initial-scale=1.0">' + '\n' + "<link rel='stylesheet' href='C:/Users/Ben/Desktop/_Coding BootCamp/_weeks lessons/week 10/EmployeeData/Assets/CSS/Questionaire.css'>" + '\n' + '<title>' + fileName + '</title>' + '\n' + '</head>'
-
-        // this sets up the inputted answers into an HTML format
         const webContent = '\n' + '<div>' + '<h1>' + nameEmp + '<h1>' + '\n' + '<p>' + 'age: ' + ageEmp + '<p>' + '\n' + '<p>' + 'height: ' + htEmp + '<p>' + '\n' + '<p>' + 'position: ' + jobEmp + '<p>' + '\n' + '</div>';
 
-        //  this combines the basic html format with the inputted answers
-        let fullstring = htmlBase + '\n' + webContent
-
-        // this determines if the base html code is needed or not so as not to multiples per file
-    //    if (answers.items[i].html_base === true) {
-    //         fullstring = htmlBase + '\n' + webContent
-    //    }
-    //    else {
-    //        fullstring = '\n' + webContent
-    //    }
 
         console.log(answers)
         // this writes the data to the file and can add to it if the program is run again
-        fs.appendFile(fileName, fullstring, function (err) {
+        fs.appendFile(fileName, webContent, function (err) {
             if (err) throw err;
             console.log('saved')
     })
